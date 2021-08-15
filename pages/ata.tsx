fe17cd01bd67352d3aa082a17dd20c86c9df323b
Atamurad <atamurad.babakulyyev@gmail.com>
2021-08-15 16:23:08 -0400

    Create new page on pages folder

---
+++ pages/ata.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type { NextPage } from 'next'
+
+const Ata: NextPage = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Atamurad</h1>
+    </div>
+  )
+}
+
+export default Ata
