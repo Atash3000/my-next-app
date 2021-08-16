06f65f85af4d95e1d807675490ad79f91f6685dd
Atamurad <atamurad.babakulyyev@gmail.com>
2021-08-15 21:23:26 -0400

    implement server connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+var cors = require('cors')
+
+
+
 
 const app = express();
+app.use(cors())
+
+
+
+
 app.get('/', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the srver'
+    message: 'Hello from the firrst server slashh'
   })
 });
+app.get('/api/v1/', (req, res) => {
+  res.status(200).json({
+    message: 'SALAm',
+  })
+})
+
 
 
 const PORT = 5050 || process.env.PORT;
-app.listen(PORT,()=>console.log('server started on port '+ PORT))
+app.listen(PORT, () => console.log('server started on port ' + PORT))
+module.exports = app;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api',
+        destination: 'http://localhost:5050/', // Proxy to Backend
+      },
+    ]
+  },
 }
